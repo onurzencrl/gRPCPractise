5472c376a05193afb1fb64931df54b6730c959c9
onurz <onurz@DESKTOP-FVJLV9P>
2024-07-25 23:37:21 +0300

    gRPC veri iletişim türleri konusu işlendi.

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using grpcMessageClient;
@@ -12,16 +13,74 @@ namespace grpcClient
             var channel = GrpcChannel.ForAddress("http://localhost:5214");
             //var greetClient = new Greeter.GreeterClient(channel);
             var messageClient = new Message.MessageClient(channel);
-            MessageResponse response = await messageClient.SendMessageAsync(new MessageRequest
+            //Unary
+            //MessageResponse response = await messageClient.SendMessageAsync(new MessageRequest
+            //{
+            //    Message = "Merhaba",
+            //    Name = "Onur"
+            //});
+
+            //Server Streaming
+            //var response = messageClient.SendMessage(new MessageRequest
+            //    {
+            //    Message = "Merhaba",
+            //    Name = "Gençay"
+            //    });
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            //while (await response.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            //{
+            //    System.Console.WriteLine(response.ResponseStream.Current.Message);
+            //}
+
+            //Client Streaming
+           // var request = messageClient.SendMessage();
+           // for (int i = 0; i < 10; i++)
+           // {
+           //     await Task.Delay(1000);
+           //     await request.RequestStream.WriteAsync(new MessageRequest
+           //     {
+           //         Message = "Merhaba",
+           //         Name = "Onur"
+           //     });
+           // }
+
+           // // Stream datanın bittiğini ifade eder.
+           //await request.RequestStream.CompleteAsync();
+
+           // Console.WriteLine((await request.ResponseAsync).Message);
+
+
+            //Bi-Directional Streaming
+
+            var request = messageClient.SendMessage();
+            var task1 = Task.Run(async () =>
             {
-                Message = "Merhaba",
-                Name = "Onur"
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    await request.RequestStream.WriteAsync(new MessageRequest
+                    {
+                        Message = "Merhaba",
+                        Name = "Onur"
+                    });
+                }
+
+            
             });
+
+            while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+            {
+                System.Console.WriteLine(request.ResponseStream.Current.Message);
+            }
+
+            await task1;
+            await request.RequestStream.CompleteAsync();
+
             //HelloReply result = await greetClient.SayHelloAsync(new HelloRequest
             //{
             //    Name = "Onur'dan selamlar"
             //});
-            System.Console.WriteLine(response.Message);
+            //System.Console.WriteLine(response.Message);
         }
     }
 }
--- grpcServer/Services/MessageService.cs
+++ grpcServer/Services/MessageService.cs
@@ -6,12 +6,66 @@ namespace grpcServer.Services;
 
 public class MessageService  : Message.MessageBase
 {
-    public override async Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+    //UNARY
+    //public override async Task<MessageResponse> SendMessage(MessageRequest request, ServerCallContext context)
+    //{
+    //    return await Task.FromResult(new MessageResponse
+    //    {
+    //        Message = $"Merhaba {request.Name}"
+    //    });
+    //}
+
+    //Server Streaming
+    //public override async Task SendMessage(MessageRequest request, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+    //{
+    //    System.Console.WriteLine($"Message : {request.Message} | Name : {request.Name}");
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        await Task.Delay(200);
+    //        await responseStream.WriteAsync(new MessageResponse
+    //        {
+    //            Message = "Merhaba " + i
+    //        });
+
+    //    }
+    //}
+
+    //Client Streaming
+    //public override async Task<MessageResponse> SendMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+    //{
+    //    while (await requestStream.MoveNext(context.CancellationToken))
+    //    {
+    //        System.Console.WriteLine($"Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+    //    }
+
+    //    return new MessageResponse
+    //        {
+    //        Message = "Veri alýnmýþtýr..."
+    //    };
+    //}
+
+    //Bi-Directional Streaming
+    public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
     {
-        return await Task.FromResult(new MessageResponse
+        var task1 = Task.Run(async () =>
         {
-            Message = $"Merhaba {request.Name}"
+            while (await requestStream.MoveNext(context.CancellationToken))
+            {
+                System.Console.WriteLine($"Message : {requestStream.Current.Message} | Name : {requestStream.Current.Name}");
+            }
+
         });
+
+        for (int i = 0; i < 10; i++)
+        {
+            await Task.Delay(200);
+            await responseStream.WriteAsync(new MessageResponse
+            {
+                Message = "Merhaba " + i
+            });
+
+        }
+        await task1;
     }
 
 }
